f44d4f036b19932e441957add1bdedaa3e168562
AnonymOwl <severinefebvet@gmail.com>
2018-01-23 23:51:28 +0100

    Simple system to get the patterns

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+
+	public int circleNumber;
+
+	private bool isActive = false;
+
+	void Awake()
+	{
+		//LineManager lm = GameObject.Find("Line").GetComponent<LineManager>();
+		LineManager.EventLineEnd += new LineEndHandler(Deactivate);
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" && other.GetComponent<LineManager>().isDrawing)
+			Activate();
+	}
+
+	void Activate()
+	{
+		transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0.3f, 1f, 0.3f); 
+		isActive = true;
+	}
+
+	void Deactivate()
+	{
+		transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f); 
+		isActive = false;
+	}
+}
---
+++ Assets/Scripts/LineManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void LineEndHandler();
+
+public class LineManager : MonoBehaviour {
+
+	private TrailRenderer tr;
+	[HideInInspector]
+	public bool isDrawing = false;
+
+	private List<int> circleList;
+
+	public static event LineEndHandler EventLineEnd;
+
+	void Start() {
+		tr = GetComponent<TrailRenderer>();		
+		circleList = new List<int>();	
+	}
+	
+	void Update() {
+		if (Input.touchCount > 0)
+		{
+			Touch currentTouch = Input.GetTouch(0);
+			// Move the "finger" object
+			if ((currentTouch.phase == TouchPhase.Began || currentTouch.phase == TouchPhase.Moved))
+			{
+				var newPosition = Camera.main.ScreenToWorldPoint(new Vector3(currentTouch.position.x, currentTouch.position.y, 0));
+				transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+			}
+
+			//Starts/Ends the line drawing
+			if (currentTouch.phase == TouchPhase.Began)
+			{
+				isDrawing = true;
+				circleList.Clear();
+			}
+			if (currentTouch.phase == TouchPhase.Ended || currentTouch.phase == TouchPhase.Canceled)
+			{
+				isDrawing = false;
+				//TODO: add an actual circleList action.
+				circleList.Clear();
+				tr.time = 0.2f;
+				OnLineEnd();
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Circle" && isDrawing)
+		{
+			if (circleList.Count == 0)
+				tr.time = 5f;
+			circleList.Add(other.GetComponent<Circle>().circleNumber);
+		}
+	}
+
+	public static void OnLineEnd()
+	{
+		if (EventLineEnd != null)
+		{
+			EventLineEnd();
+		}
+	}
+}
