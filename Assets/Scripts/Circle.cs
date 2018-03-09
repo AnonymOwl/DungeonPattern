02b896c6e6220a382ed342cc2547cca2426b38de
Severine Febvet <severinefebvet@gmail.com>
2018-03-09 17:47:26 +0100

    vibration when circle is activated, better circle sprite, circle activation effect, and finished character model with UV and textures

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
+
 
 public class Circle : MonoBehaviour {
 
 	public int circleNumber;
 
 	private bool isActive = false;
+	private AndroidJavaObject vibrator;
+	private long vibrationDuration = 50;
+	private ParticleSystem effectSystem;
+	private SpriteRenderer sprite;
 
 	void Awake()
 	{
-		//LineManager lm = GameObject.Find("Line").GetComponent<LineManager>();
 		LineManager.EventLineEnd += new LineEndHandler(Deactivate);
-	}
 
+		effectSystem = GetComponentInChildren<ParticleSystem>();
+		sprite = GetComponentInChildren<SpriteRenderer>();
+		if (Application.platform == RuntimePlatform.Android) {
+			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+			vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
@@ -23,13 +36,21 @@ public class Circle : MonoBehaviour {
 
 	void Activate()
 	{
-		transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0.3f, 1f, 0.3f); 
-		isActive = true;
+		if (!isActive) {
+			if (Application.platform == RuntimePlatform.Android)
+				vibrator.Call("vibrate", vibrationDuration);
+			if (effectSystem != null)
+				effectSystem.Play();
+			sprite.color = new Color(0.3f, 1f, 0.3f); 
+			isActive = true;
+		}
 	}
 
 	void Deactivate()
 	{
-		transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f); 
-		isActive = false;
+		if (isActive) {
+			sprite.color = new Color(1f, 1f, 1f); 
+			isActive = false;
+		}
 	}
 }
